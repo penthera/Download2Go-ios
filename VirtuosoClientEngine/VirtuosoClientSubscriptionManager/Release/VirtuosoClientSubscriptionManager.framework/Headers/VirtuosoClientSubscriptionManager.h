6239c0b7fb14dea7dae135432872ba2665623f6e
Josh Pressnell <josh@penthera.com>
2018-04-30 11:10:34 -0400

    Support for direct use of Uplynk asset URLs.  Skip size validation for content returned with compressed encodings.

---
+++ VirtuosoClientEngine/VirtuosoClientSubscriptionManager/Release/VirtuosoClientSubscriptionManager.framework/Headers/VirtuosoClientSubscriptionManager.h
@@ -0,0 +1,37 @@
+/*!
+ *  @header Virtuoso Client Subscription Manager
+ *
+ *  PENTHERA CONFIDENTIAL
+ *
+ *  Notice: This file is the property of Penthera Inc.
+ *  The concepts contained herein are proprietary to Penthera Inc.
+ *  and may be covered by U.S. and/or foreign patents and/or patent
+ *  applications, and are protected by trade secret or copyright law.
+ *  Distributing and/or reproducing this information is forbidden unless
+ *  prior written permission is obtained from Penthera Inc.
+ *
+ *  @copyright (c) 2018 Penthera Inc. All Rights Reserved.
+ *
+ */
+
+#ifndef VirtuosoClientSubscriptionManager_Master_h
+#define VirtuosoClientSubscriptionManager_Master_h
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VirtuosoClientSubscriptionManager.
+FOUNDATION_EXPORT double VirtuosoClientSubscriptionManagerVersionNumber;
+
+//! Project version string for VirtuosoClientSubscriptionManager.
+FOUNDATION_EXPORT const unsigned char VirtuosoClientSubscriptionManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VirtuosoClientSubscriptionManager/PublicHeader.h>
+#pragma mark
+#pragma mark Subscription SDK
+#pragma mark
+
+#import <VirtuosoClientSubscriptionManager/VirtuosoSubscriptionDataSource.h>
+#import <VirtuosoClientSubscriptionManager/VirtuosoSubscriptionDefaultDataSource.h>
+#import <VirtuosoClientSubscriptionManager/VirtuosoSubscriptionManager.h>
+
+#endif
