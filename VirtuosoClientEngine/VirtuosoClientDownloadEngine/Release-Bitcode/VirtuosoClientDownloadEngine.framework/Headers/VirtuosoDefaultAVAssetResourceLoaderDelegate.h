85d004999a8aca2be391a697102d92a21efdf1ab
Josh Pressnell <josh.pressnell@penthera.com>
2019-09-25 17:04:08 -0400

    Version 3.15.6 update.  See release notes.

--- VirtuosoClientEngine/VirtuosoClientDownloadEngine/Release-Bitcode/VirtuosoClientDownloadEngine.framework/Headers/VirtuosoDefaultAVAssetResourceLoaderDelegate.h
+++ VirtuosoClientEngine/VirtuosoClientDownloadEngine/Release-Bitcode/VirtuosoClientDownloadEngine.framework/Headers/VirtuosoDefaultAVAssetResourceLoaderDelegate.h
@@ -49,7 +49,47 @@
  */
 @protocol VirtuosoLicenseProcessingDelegate <NSObject>
 
+/*!
+ *  @abstract Allows a custom CID to be used with the SDK default AVAssetResourceLoaderDelegate
+ *
+ *  @discussion Normally, the asset CID is extracted from the FairPlay license URL, which takes the form
+ *              skd://<contentID>.  Some license servers put the required content ID elsewhere.  If you
+ *              don't need to adjust the content ID, you can return nil and the SDK will use the default value
+ *              it detects.  If you need to override this value, you can return the proper content ID to use from
+ *              this method.
+ *
+ *  @param asset The VirtuosoAsset the licensing request is for
+ *  @param fpRequest The URL provided to the AVAssetResourceLoaderDelegate for the licensing request
+ *  @return The CID value to use during SPC creation
+ */
+- (NSString*)extractCIDForAsset:(VirtuosoAsset*)asset fromFairPlayRequest:(NSURL*)fpRequest;
+
+/*!
+ *  @abstract Allows custom processing of the FairPlay POST body before it is send.
+ *
+ *  @discussion Normally, the SPC data is posted as binary as the POST body to the fairplay server.
+ *              If this is the case, you can just return the spc input without modification.  If your
+ *              server requires some custom format (base64 encoding or JSON wrapping, for example), then you
+ *              should receive the input SPC and return an NSData object suitable for posting in the POST body
+ *              to your licensing server.
+ *
+ *  @param spc The Apple-generated SPC data value
+ *  @return An NSData object suitable for the license server POST body
+ */
 - (NSData*)prepareSPCForLicenseRequest:(NSData*)spc;
+
+/*!
+ *  @abstract Allows custom processing of the FairPlay license server response
+ *
+ *  @discussion Normally, the license server responds to a FairPlay request with binary representing
+ *              the license response (CKC).  Some license servers wrap this CKC data with other encodings,
+ *              such as base64 or JSON wrappers.  If your license server returns something other than raw
+ *              CKC data, you will need to implement this method.  You should parse the response data and return
+ *              the raw CKC data object, suitable for handing back to the player in the AVASsetResourceLoaderDelegate response.
+ *
+ *  @param response The received response from the license server
+ *  @return CKC data value suitable for handing to the player for DRM licensing
+ */
 - (NSData*)extractCKCFromLicenseResponse:(NSData*)response;
 
 @end
