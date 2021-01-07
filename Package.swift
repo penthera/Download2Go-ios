ab8d1f9618a1daf94f4a3638c8d6bbfdbb905315
markstevenlee <54903087+markstevenlee@users.noreply.github.com>
2021-01-07 13:08:26 -0500

    Update Package.swift
    Correct checksum.

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         .binaryTarget(
             name: "VirtuosoClientDownloadEngine",
             url: "https://github.com/penthera/Download2Go-ios/releases/download/v4.1/VirtuosoClientDownloadEngine.xcframework.zip",
-            checksum: "6ebbc49ae7ba8feb980890811a87dba6e02835d19c8d67f697125b0baef82841"
+            checksum: "be6ab8ccf652cd72cbf16bcf63867f5cdaab8024eaeae32b6fb3e06dc2d59b4d"
         )
     ]
 )
