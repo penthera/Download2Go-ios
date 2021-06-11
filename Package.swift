da9618a994126820b36b73bd1d76dba40de89898
markstevenlee <54903087+markstevenlee@users.noreply.github.com>
2021-06-11 15:06:03 -0400

    Update Package.swift
    Correct release version.

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "VirtuosoClientDownloadEngine",
-            url: "https://github.com/penthera/Download2Go-ios/releases/download/v4.2/VirtuosoClientDownloadEngine-spm.xcframework.zip",
+            url: "https://github.com/penthera/Download2Go-ios/releases/download/v4.2.0/VirtuosoClientDownloadEngine-spm.xcframework.zip",
             checksum: "c42bf14dc894393940155a26577c0ac32614cc5831f52b9d9c6fbc0da400968d"
         )
     ]
