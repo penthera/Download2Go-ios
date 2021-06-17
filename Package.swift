f6b81cd7ef2178042b72d1e90b1db8fbe84cf2f8
Mark S. Lee <mark.lee@penthera.com>
2021-06-17 15:41:13 -0400

    Release v4.2.0.1

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "VirtuosoClientDownloadEngine",
-            url: "https://github.com/penthera/Download2Go-ios/releases/download/v4.2/VirtuosoClientDownloadEngine-spm.xcframework.zip",
-            checksum: "c42bf14dc894393940155a26577c0ac32614cc5831f52b9d9c6fbc0da400968d"
+            url: "https://github.com/penthera/Download2Go-ios/releases/download/v4.2.0.1/VirtuosoClientDownloadEngine-spm.xcframework.zip",
+            checksum: "dd275dc2a40daa49d54e075a23bcbbae907a4cb5a2a46984803df522bd2ad61b"
         )
     ]
 )
