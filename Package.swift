7cec102feebdb88ea7ece9fb564172b899ad5198
Josh Pressnell <josh.pressnell@penthera.com>
2021-07-08 16:03:59 -0400

    Corrected Hotfix 4.2.0.3

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         .binaryTarget(
             name: "VirtuosoClientDownloadEngine",
             url: "https://github.com/penthera/Download2Go-ios/releases/download/v4.2.0.3/VirtuosoClientDownloadEngine-spm.xcframework.zip",
-            checksum: "983e8eee0aa367cc1b94fb4d4d981566e8f7f013030f6830d77285b3dd7277a3"
+            checksum: "09c2c45559bb71d906a1a56c34321d2154e67473ff17d185fab7a1de1d645fe7"
         )
     ]
 )
