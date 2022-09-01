a50aef83b9f5d5596a27a41adc68764fbba73eb0
penthera-bots <josh.pressnell@penthera.com>
2022-09-01 17:32:17 -0400

    Version 4.2.24 Release

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "VirtuosoClientDownloadEngine",
-            url: "https://github.com/penthera/Download2Go-ios/releases/download/v4.2.23/VirtuosoClientDownloadEngine-spm.xcframework.zip",
-            checksum: "09435eb285c4d047ee305fbf9459276158b584fb8f517b553b1aa33ffa1b4e13"
+            url: "https://github.com/penthera/Download2Go-ios/releases/download/v4.2.24/VirtuosoClientDownloadEngine-spm.xcframework.zip",
+            checksum: "20988a91d592964151af8e6136456133683ba056166319ee788120a171d57b01"
         )
     ]
 )
